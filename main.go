b6ec16b6b7f6474756fef593d7c593a48408664f
larmic <mail@larmic.de>
2025-04-23 20:39:09 +0200

    Add Prometheus metrics integration with middleware support
    Introduced Prometheus for monitoring HTTP requests, including metrics for request counts and latencies. Updated the router to include a metrics middleware and exposed a new `/metrics` endpoint. Dependencies and documentation were also updated to support this feature.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	api "abfallkalender_api/src/backend"
 	_ "embed"
 	"github.com/gorilla/handlers"
+	"github.com/prometheus/client_golang/prometheus"
 	"log"
 	"net/http"
 	"os"
@@ -15,10 +16,31 @@ var kalenderJS string
 // //go:embed dist/kalender.js.map
 var kalenderJSMap string
 
+var (
+	requestCount = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Total number of HTTP requests",
+		},
+		[]string{"method", "endpoint"},
+	)
+	requestLatency = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "http_request_duration_seconds",
+			Help:    "HTTP request latency in seconds",
+			Buckets: prometheus.DefBuckets,
+		},
+		[]string{"method", "endpoint"},
+	)
+)
+
 func main() {
 	log.Println("Hello Bremer Abfallkalender API!!!")
 
-	router := api.NewRouter(kalenderJS, kalenderJSMap)
+	prometheus.MustRegister(requestCount)
+	prometheus.MustRegister(requestLatency)
+
+	router := api.NewRouter(kalenderJS, kalenderJSMap, requestCount, requestLatency)
 
 	// TODO use os.lookup env
 	port, portSet := os.LookupEnv("PORT")
--- src/backend/router.go
+++ src/backend/router.go
@@ -2,14 +2,18 @@ package api
 
 import (
 	"abfallkalender_api/src/backend/handler"
+	"abfallkalender_api/src/backend/handler/middleware"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
-func NewRouter(kalenderJS string, kalenderJSMap string) *mux.Router {
+func NewRouter(kalenderJS string, kalenderJSMap string, requestCount *prometheus.CounterVec, requestLatency *prometheus.HistogramVec) *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
 	router.NotFoundHandler = handler.Handle404()
+
+	router.Use(middleware.MetricsMiddleware(requestCount, requestLatency))
 
 	// TODO signal handler
 
